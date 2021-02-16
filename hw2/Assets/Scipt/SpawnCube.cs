ef69f178e7a9b963de5de68aa4d8fda39debafda
niall-healy <niallhealy17@gmail.com>
2021-02-15 22:46:23 -0800

    add ch scene with cube spawning and collision sound

---
+++ hw2/Assets/Scipt/SpawnCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour{
+
+    public GameObject cube;
+
+    public void Spawn(Vector3 position) {
+        Instantiate(cube).transform.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            Spawn(Vector3.zero);
+        }
+    }
+}
