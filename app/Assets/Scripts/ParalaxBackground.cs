8e2f6ec634b6bf36b19564090972e40331c1f15c
niall-healy <niallhealy17@gmail.com>
2021-03-29 21:56:48 -0700

    basic tutorial level and hacky menu implementation

---
+++ app/Assets/Scripts/ParalaxBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour {
+
+    public float paralaxMultiplier = .5f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    void Start() {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate() {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * paralaxMultiplier;
+        lastCameraPosition = cameraTransform.position;
+
+    }
+}
