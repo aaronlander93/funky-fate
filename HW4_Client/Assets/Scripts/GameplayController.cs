a14202aea50c2ad0dece608402ce72872cbe105d
MAbril1 <mabril0526@gmail.com>
2021-03-29 18:55:31 -0700

    Scoring done
    need to add protocols and menu for players

--- HW4_Client/Assets/Scripts/GameplayController.cs
+++ HW4_Client/Assets/Scripts/GameplayController.cs
@@ -21,9 +21,17 @@ public class GameplayController : MonoBehaviour
     [SerializeField]
     private Text info;
 
+    [SerializeField]
+    private Text p1_score_text, p2_score_text;
+
     private GameChoices p1_choice = GameChoices.NONE, p2_choice = GameChoices.NONE;
 
     private AnimationController animationController;
+
+    private int max_score = 3;
+
+    private int p1_id, p2_id;
+    private int p1_score = 0, p2_score = 0;
 
     void Awake() {
         animationController = GetComponent<AnimationController>();
@@ -72,28 +80,46 @@ public class GameplayController : MonoBehaviour
         if (p1_choice == p2_choice) {
             //draw
             info.text = "DRAW";
-            StartCoroutine(DisplayWinnerAndRestart());
+            StartCoroutine(DisplayWinnerAndMoveOn(0));
             return;
         }
         else if ((p1_choice == GameChoices.ROCK && p2_choice == GameChoices.SCISSORS) ||
             (p1_choice == GameChoices.PAPER && p2_choice == GameChoices.ROCK) ||
             (p1_choice == GameChoices.SCISSORS && p2_choice == GameChoices.PAPER)) {
             info.text = "You WIN!";
-            StartCoroutine(DisplayWinnerAndRestart());
+            StartCoroutine(DisplayWinnerAndMoveOn(1));
             return;
         }
         else {
             info.text = "You LOSE";
-            StartCoroutine(DisplayWinnerAndRestart());
+            StartCoroutine(DisplayWinnerAndMoveOn(2));
             return;
         }
     }
 
-    IEnumerator DisplayWinnerAndRestart() {
+    IEnumerator DisplayWinnerAndMoveOn(int player) {
         yield return new WaitForSeconds(2f);
         info.gameObject.SetActive(true);
-        yield return new WaitForSeconds(2f);
-        info.gameObject.SetActive(false);
-        animationController.ResetAnimations();
+        if (player == 1) {
+            p1_score++;
+            p1_score_text.text = "Your score: " + p1_score;
+        }
+        else if (player == 2) {
+            p2_score++;
+            p2_score_text.text = "Their score: " + p2_score;
+        }
+        if(p1_score == max_score){
+            //player 1 wins
+            info.text = "You are Victorious!";
+        }
+        else if(p2_score == max_score){
+            //player 2 wins
+            info.text = "You are the Loser...";
+        }
+        else {
+            yield return new WaitForSeconds(2f);
+            info.gameObject.SetActive(false);
+            animationController.ResetAnimations();
+        }
     }
 }
