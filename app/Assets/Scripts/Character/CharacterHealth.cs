8d1fe475f5bc184fd8cb4ad5ffe466f1db82d4ed
Aaron Lander <36257200+aaronlander93@users.noreply.github.com>
2021-04-03 13:42:22 -0700

    Reorganized file structure. Added character attack. Added knock back for enemies when attacked.

---
+++ app/Assets/Scripts/Character/CharacterHealth.cs
@@ -0,0 +1,45 @@
+﻿/*
+Code By: Aaron Lander
+This script controls and keeps track of the character's health.
+Whenever the character takes damage, a call is made to the HealthBar
+object to change the current health bar image on the UI. 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterHealth : MonoBehaviour
+{
+    [SerializeField] private int characterHealth = 5;
+    private bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        characterHealth--;
+
+        if(characterHealth == 0)
+        {
+            // Death animation will go here in the future
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetCharacterHealth()
+    {
+        return characterHealth;
+    }
+}
