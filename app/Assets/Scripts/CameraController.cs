3a3346d0d96580ebfe40b83ea9c5f199f003b9b5
niall-healy <niallhealy17@gmail.com>
2021-04-03 18:42:42 -0700

    add camera smoothing and tweak movement paramaters

--- app/Assets/Scripts/CameraController.cs
+++ app/Assets/Scripts/CameraController.cs
@@ -1,6 +1,10 @@
 ﻿/*
 Code By: Andrew Sha
 This code makes the camera follow a 2D entity around.
+
+Edited by: Niall Healy
+Added camera smoothing based off this tutorial:
+https://www.youtube.com/watch?v=MFQhpwc6cKE
 */
 
 using System.Collections;
@@ -10,7 +14,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Movement2D player;
-
+    public float smoothSpeed = 0.125f;
     public bool isFollowing;
 
     public float xOffSet;
@@ -23,12 +27,15 @@ public class CameraController : MonoBehaviour
         isFollowing = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Using FixedUpdate here instead of Update to ensure no race condition between
+    // player and camera movement
+    void FixedUpdate()
     {
         if (isFollowing)
         {
-            transform.position = new Vector3(player.transform.position.x + xOffSet, player.transform.position.y + yOffSet, transform.position.z);
-                };
+            Vector3 desiredPosition = new Vector3(player.transform.position.x + xOffSet, player.transform.position.y + yOffSet, transform.position.z);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        };
     }
 }
