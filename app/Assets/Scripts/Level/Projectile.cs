6cf0b3a79c68f931238fb17970fa582c40dac02c
MAbril1 <mabril0526@gmail.com>
2021-04-08 16:12:04 -0700

    projectiles fly at closest player's direction
    projectile destroys on player collision
    need to destroy on ground or wall collision
    need to hit damage player on collision

--- app/Assets/Scripts/Level/Projectile.cs
+++ app/Assets/Scripts/Level/Projectile.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    Rigidbody2D _rb;
     private GameObject[] players;
     private GameObject closestPlayer;
+
+    private Vector2 direction;
+    public float moveSpeed;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        players = GameObject.FindGameObjectsWithTag("Player");
         FindNearestPlayer();
         
+        direction = (closestPlayer.transform.position - transform.position).normalized * moveSpeed;
+        _rb.velocity = new Vector2(direction.x, direction.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(closestPlayer == null)
+        {
+            return;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag =="Player")
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "ground")
+        {
+            Debug.Log("Hit the ground");
+            Destroy(gameObject);
+        }
+ 
     }
 
     private void FindNearestPlayer()
