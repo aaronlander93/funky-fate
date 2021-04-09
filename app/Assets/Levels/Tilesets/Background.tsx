20a70aa6bfc0e0450c61cab3d62815452791d1b6
JosephBabel <theodorebabel@gmail.com>
2021-04-09 04:27:59 -0700

    add multiple parallax layers to tiled map with new buildings tilesheet

---
+++ app/Assets/Levels/Tilesets/Background.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Background" tilewidth="16" tileheight="16" tilecount="6272" columns="112">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="spritesheet-buildings.png" width="1792" height="896"/>
+</tileset>
