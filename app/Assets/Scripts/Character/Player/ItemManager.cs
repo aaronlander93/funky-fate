76c4d7584b7277fd0e747ef553e37aa2a9150f88
JosephBabel <theodorebabel@gmail.com>
2021-04-17 00:23:56 -0700

    add key follow to player on pickup

---
+++ app/Assets/Scripts/Character/Player/ItemManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    private bool hasKey = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Key")
+        {
+            hasKey = true;
+            collision.gameObject.GetComponent<ItemPickup>().AnimateItemPickup(gameObject);
+
+        }
+    }
+}
---
+++ app/Assets/Scripts/Items/ItemPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isPickedUp = false;
+    private bool isFollowing = false;
+    private GameObject player;
+
+    public void AnimateItemPickup(GameObject player)
+    {
+        if (isPickedUp == false)
+        {
+            rb = (Rigidbody2D)gameObject.AddComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(0, 10.0f), ForceMode2D.Impulse);
+            isPickedUp = true;
+            StartCoroutine(StartFollow());
+        }
+
+        this.player = player;
+    }
+
+    IEnumerator StartFollow()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.layer = 11;
+        rb.gravityScale = 0.0f;
+        isFollowing = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (isFollowing)
+        {
+            rb.AddForce((player.transform.position - transform.position) * 15.0f);
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, 5.0f);
+        }
+    }
+}
