572bce264304347ae5665175f61d8c9c03bdda07
Aaron Lander <36257200+aaronlander93@users.noreply.github.com>
2021-04-17 18:56:18 -0700

    Added chat to multiplayer.

--- app/Assets/Scripts/GameConfig.cs
+++ app/Assets/Scripts/GameConfig.cs
@@ -6,4 +6,7 @@ public static class GameConfig
 {
     private static bool multiplayer = false;
     public static bool Multiplayer { get; set; }
+
+    private static string nickname;
+    public static string Nickname { get; set; }
 }
