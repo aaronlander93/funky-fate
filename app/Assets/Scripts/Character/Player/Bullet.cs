f16dc28a1892848545676e2d24d4f856cf866f52
andrewsha098 <andrewsha098@gmail.com>
2021-05-06 02:36:13 -0700

    made some bullets

---
+++ app/Assets/Scripts/Character/Player/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet: MonoBehaviour
+{
+    public int damage = 1;
+    public float speed = 10f;
+    public float lifeTime = 1f;
+    public Rigidbody2D rb;
+    public GameObject Effect;
+    // Start is called before the first frame update
+    private GameSetupController gsc;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Invoke("Particles",lifeTime);
+    }
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage, true);
+        }
+        Instantiate(Effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void Particles(){
+        Instantiate(Effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
