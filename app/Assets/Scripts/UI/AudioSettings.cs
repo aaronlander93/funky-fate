c0062a56b3a7df6f4ee5ea3b624f2db296316767
niall-healy <niallhealy17@gmail.com>
2021-05-07 20:41:48 -0700

    added music and master volume to GameConfig.cs so volumes persist between scenes

--- app/Assets/Scripts/UI/AudioSettings.cs
+++ app/Assets/Scripts/UI/AudioSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
 Author: Niall (sort-of. I stole it from https://www.youtube.com/watch?v=yQgVKR6PMqo)
@@ -12,24 +13,34 @@ public class AudioSettings : MonoBehaviour {
     FMOD.Studio.Bus Music;
     FMOD.Studio.Bus Master;
 
-    float MusicVolume = 0.5f;
-    float MasterVolume = 1f;
+    float musicVolume = 0.5f;
+    float masterVolume = 1f;
+
+    public Slider masterSlider;
+    public Slider musicSlider;
 
     void Awake () {
         Music = FMODUnity.RuntimeManager.GetBus ("bus:/Master/Music");
         Master = FMODUnity.RuntimeManager.GetBus ("bus:/Master");
+
+        masterSlider.value = GameConfig.MasterVolume;
+        musicSlider.value = GameConfig.MusicVolume;
     }
 
     void Update () {
-        Music.setVolume (MusicVolume);
-        Master.setVolume (MasterVolume);
+        Music.setVolume (musicVolume);
+        Master.setVolume (masterVolume);
     }
 
     public void MasterVolumeLevel (float newMasterVolume) {
-        MasterVolume = newMasterVolume;
+        masterVolume = newMasterVolume;
+
+        GameConfig.MasterVolume = newMasterVolume;
     }
 
     public void MusicVolumeLevel (float newMusicVolume) {
-        MusicVolume = newMusicVolume;
+        musicVolume = newMusicVolume;
+
+        GameConfig.MusicVolume = newMusicVolume;
     }
 }
